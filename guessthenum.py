67693db9b166cf5339611f1dc264ffed0ed43f46
SayaliKarnewar <33855068+sayalikarnewar@users.noreply.github.com>
2018-12-30 14:05:30 +0530

    guessthenum.py

--- guessthenum.py
+++ guessthenum.py
@@ -1,18 +1,19 @@
 #https://knightlab.northwestern.edu/2014/06/05/five-mini-programming-projects-for-the-python-beginner/
+
 import random
-n=1
-def guess():
-	global n
-	n = random.randrange(1,100,1)
-	user_input=int(input("your input"))
 
-for i in range(100):
-	global n
-	if (n-user_input)==i:
-		print("digits far",i)
-		guess()
+def func():
+	n = random.randrange(100)
+	def guess():
+		m =int(input("guess the number"))
+		if n==m:
+			print ("correct guess. yipeee")
+		elif n>m:
+			print ("guess higher ")
+			guess()
+		elif n<m:
+			print ("guess lower")
+			guess()
+	guess()
+func()
 
-#	print("upto 10 digits far")
-#	guess()
-#	print("upto 20 digits far")
-#	guess()
